c36713530e4b11755f22822539961f40be89a7be
Richar Urbano <richar.urbano@gmail.com>
2018-10-25 22:49:26 -0500

    Add mockup v.2

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,23 @@
-$(".toggle-sidebar").on("click", function() {
-    $(".left-aside").toggleClass("left-aside-collapse");
+$(document).ready(function () {
+
+    // MENU ACTIONS
+    $(".toggle-sidebar").click(function (event) {
+        event.preventDefault();
+        if (!$(".left-aside").hasClass("aside-close")) {
+            $(".left-aside").toggleClass("aside-close");
+            $(".left-aside").animate({
+                flex: "0 0 50px"
+            }, function () {
+                $(".main-section").toggleClass("col-11");
+            });
+        } else {
+            $(".main-section").toggleClass("col-11");
+            $(".left-aside").animate({
+                flex: "0 0 25%"
+            }, function () {
+                $(this).toggleClass("aside-close");
+            });
+        }
+    });
+
 });
